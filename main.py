213a10b30986bc82519f041e62f6e84838d067cc
Roh <rohisenthil15@gmail.com>
2026-07-21 14:41:58 +1000

    fix: add CORS middleware for react frontend
    The browser did not allow react frontend to read the
    request output. Now, with added CORS middleware, everything should
    work

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ from bootstrap_routes import router as bootstrap_router
 from game_routes import router as game_router
 from auth_routes import router as auth_router
 from users_routes import router as user_router
+from fastapi.middleware.cors import CORSMiddleware
 
 
 
@@ -15,8 +16,20 @@ from users_routes import router as user_router
 load_dotenv()
 
 app = FastAPI()
-#app.mount("/sandboxes_data",StaticFiles(directory="sandboxes_data"), name="sandboxes")
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        "http://127.0.0.1:5173",
+        "http://localhost:5173",
+        # Add deployed React URL here.
+    ],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+#app.mount("/sandboxes_data",StaticFiles(directory="sandboxes_data"), name="sandboxes")
 app.include_router(sandbox_router)
 app.include_router(bootstrap_router)
 app.include_router(game_router)
